01b72cd7999bb0bc7d47829e33fa4d55f931b0d2
Richie Tauch <rtauc001@gmail.com>
2021-07-29 17:59:39 -0700

    initial commit. Initial styling

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+const Header = () => {
+    return (
+        <header className='App-title'>
+            <a href='/Home' className='home'>Home</a>
+            <h1 className='centerTitle'>\\Tech Blogger//</h1>
+            <a href='/Login' className='login'>Sign in</a>
+        </header>
+    )
+}
+
+export default Header
